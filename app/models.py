faeb3eb84a38ce5f5e74fdb0d9e768990ddae7c9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-06-30 12:32:50 +1200

    changed up the whole structure of the models file and also tidied up alot of stray variables in the routes file.

--- app/models.py
+++ app/models.py
@@ -1,12 +1,5 @@
 from app.routes import db
 
-class Tutors(db.Model):
-    __tablename__ = "Tutors"
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String())
-    description = db.Column(db.String())
-    Times = db.Column(db.Integer, db.ForeignKey("Times.id"))
-    Times_day_of_week = db.relationship("Times", backref = "Tutors")
 
 class Times(db.Model):
     __tablename__ = "Times"
@@ -14,10 +7,40 @@ class Times(db.Model):
     day_of_week = db.Column(db.String())
     time_in_day = db.Column(db.Integer())
 
+SubjectLevel = db.Table('SubjectLevel', db.Model.metadata, 
+    db.Column('Subject_id', db.Integer, db.ForeignKey('Subject.id')),
+    db.Column('Level_id', db.Integer, db.ForeignKey('Level.id'))
+    )
+
+SubjectTutor = db.Table('SubjectTutor', db.Model.metadata,
+    db.Column('Subject_id', db.Integer, db.ForeignKey('Subject.id')),
+    db.Column('Tutor_id', db.Integer, db.ForeignKey('Tutor.id'))
+    )
+
+class Level(db.Model):
+    __tablename__='Level'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer)
+    description = db.Column(db.Text)
+
+    subject = db.relationship('Subject', secondary=SubjectLevel, back_populates='levels')
+
 class Subject(db.Model):
     __tablename__ = "Subject"
+
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String())
-    level = db.Column(db.Integer())
+    description = db.Column(db.String())
+
+    levels = db.relationship('Level', secondary=SubjectLevel, back_populates='subjects')
+    tutors = db.relationship('Tutor', secondary=SubjectTutor, back_populates='tutors')
 
 
+class Tutors(db.Model):
+    __tablename__ = "Tutors"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String())
+    description = db.Column(db.String())
+    
+    subject = db.relationship('Subject', secondary=SubjectTutor, back_populates='subjects')
--- app/routes.py
+++ app/routes.py
@@ -18,21 +18,31 @@ def home():
     return render_template('home.html', page_title = 'The Website')
 
 @app.route('/level/<int:level>')
-def show_level(id):
-    results = models.Subject.query.filter_by(id = id)
+def show_level(level):
+    results = models.Subject.query.filter_by(level = level).first()
     return render_template('levels.html', page_title = 'LEVEL')
     if level<1 or level>3:
         abort(404)
 
-@app.route('/Subjects/<str:name>')
-def Subjects (level):
-    Subjects = models.Subject.query.filter_by(level = level)
-    return render_template("Subjects.html", Subjects = Subjects)
+
+@app.route('/subject/<int:id>')
+def subjects(id):
+    subject = models.Subject.query.filter_by(id=id).first()
+    print (subject.name, subject.level)
+    return render_template("Subjects.html", subject=subject)
+
 
 @app.route('/all_tutors')
-def tutors():
+def all_tutors():
     results = models.Tutors.query.all()
+    print (results.name)
+    return render_template('all_tutors.html', page_title = 'ALL_TUTORS')
 
+@app.route('/tutor/<int:id>')
+def tutors(id):
+    tutor = models.Tutors.query.filter_by(id = id).first_or_404()
+    print(tutor.name, tutor.description)
+    return render_template("Tutor.html", tutor=tutor)
 
 @app.route('/contact')
 def contact ():
