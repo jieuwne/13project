8c6a8bd67fb1a7cefd540c97328872ef490ccfd3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-21 10:57:03 +1200

    cleaning up my sgl links with database and code. made sure Names of table were the same and singular.

--- app/models.py
+++ app/models.py
@@ -1,46 +1,57 @@
 from app.routes import db
 
-
-class Times(db.Model):
-    __tablename__ = "Times"
-    id = db.Column(db.Integer, primary_key = True)
-    day_of_week = db.Column(db.String())
-    time_in_day = db.Column(db.Integer())
-
 SubjectLevel = db.Table('SubjectLevel', db.Model.metadata, 
-    db.Column('Subject_id', db.Integer, db.ForeignKey('Subject.id')),
-    db.Column('Level_id', db.Integer, db.ForeignKey('Level.id'))
+    db.Column('sid', db.Integer, db.ForeignKey('Subject.id')),
+    db.Column('lid', db.Integer, db.ForeignKey('Level.id'))
     )
 
 SubjectTutor = db.Table('SubjectTutor', db.Model.metadata,
-    db.Column('Subject_id', db.Integer, db.ForeignKey('Subject.id')),
-    db.Column('Tutor_id', db.Integer, db.ForeignKey('Tutor.id'))
+    db.Column('sid', db.Integer, db.ForeignKey('Subject.id')),
+    db.Column('tid', db.Integer, db.ForeignKey('Tutor.id'))
     )
 
+TutorTime = db.Table('TutorTime', db.Model.metadata, 
+    db.Column('tuid', db.Integer, db.ForeignKey('Tutor.id')),
+    db.Column('tiid', db.Integer, db.ForeignKey('Time.id'))
+    )
+
+#linking the many to many tables in the database to the code?
+# derived from Mr Dunford's Replit page. 
+
+
+class Time(db.Model):
+    __tablename__ = "Time"
+    id = db.Column(db.Integer, primary_key=True)
+    day_of_week = db.Column(db.String())
+    time_in_day = db.Column(db.String())
+
+    tutors = db.relationship('Tutor', secondary=TutorTime, back_populates='times')
+
+
 class Level(db.Model):
-    __tablename__='Level'
+    __tablename__ = 'Level'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Integer)
-    description = db.Column(db.Text)
+    name = db.Column(db.String())
 
-    subject = db.relationship('Subject', secondary=SubjectLevel, back_populates='levels')
+    subjects = db.relationship('Subject', secondary=SubjectLevel, back_populates='levels')
+
 
 class Subject(db.Model):
     __tablename__ = "Subject"
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    description = db.Column(db.String())
 
     levels = db.relationship('Level', secondary=SubjectLevel, back_populates='subjects')
-    tutors = db.relationship('Tutor', secondary=SubjectTutor, back_populates='tutors')
+    tutors = db.relationship('Tutor', secondary=SubjectTutor, back_populates='subjects')
 
 
-class Tutors(db.Model):
-    __tablename__ = "Tutors"
-    id = db.Column(db.Integer, primary_key = True)
+class Tutor(db.Model):
+    __tablename__ = "Tutor"
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     description = db.Column(db.String())
     
-    subject = db.relationship('Subject', secondary=SubjectTutor, back_populates='subjects')
+    subjects = db.relationship('Subject', secondary=SubjectTutor, back_populates='tutors')
+    times = db.relationship('Time', secondary=TutorTime, back_populates='tutors')
--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,53 @@
 from app import app
-
-from flask import render_template
-
+from flask import render_template, abort
 from flask_sqlalchemy import SQLAlchemy 
-
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir,"Tutoring.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "tutoring.db")
 db.init_app(app)
 
 import app.models as models
 
+
 @app.route('/')
 def home():
-    return render_template('home.html', page_title = 'The Website')
+    return render_template('home.html', page_title='The Website')
+
 
 @app.route('/level/<int:level>')
 def show_level(level):
-    results = models.Subject.query.filter_by(level = level).first()
-    return render_template('levels.html', page_title = 'LEVEL')
-    if level<1 or level>3:
+    if level < 1 or level > 3:
         abort(404)
+    results = models.Subject.query.filter_by(level=level).first()
+    print(results.name)
+    return render_template('levels.html', page_title='LEVEL')
+  
 
-
-@app.route('/subject/<int:id>')
-def subjects(id):
+#@app.route('/subjects/<int:id>')
+#def subjects(id): 
     subject = models.Subject.query.filter_by(id=id).first()
-    print (subject.name, subject.level)
-    return render_template("Subjects.html", subject=subject)
+    print(subject.name, subject.level)
+    return render_template("subjects.html", subject=subject)
 
 
 @app.route('/all_tutors')
 def all_tutors():
-    results = models.Tutors.query.all()
-    print (results.name)
-    return render_template('all_tutors.html', page_title = 'ALL_TUTORS')
+    results = models.Tutor.query.all()
+    print(results.name)
+    return render_template('all_tutors.html', page_title='ALL_TUTORS')
 
 @app.route('/tutor/<int:id>')
 def tutors(id):
-    tutor = models.Tutors.query.filter_by(id = id).first_or_404()
+    tutor = models.Tutor.query.filter_by(id=id).first_or_404()
     print(tutor.name, tutor.description)
-    return render_template("Tutor.html", tutor=tutor)
+    return render_template("all_tutors.html", tutor=tutor)
 
 @app.route('/contact')
-def contact ():
-    return render_template('contact.html', page_title = 'CONTACTS')
+def contact():
+    return render_template('contact.html', page_title='CONTACTS')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
